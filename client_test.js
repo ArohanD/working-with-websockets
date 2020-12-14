0e80274a03986ed42cb3cf5eeada9f899857eca7
Arohan Dutt <arohandutt@gmail.com>
2020-12-13 21:50:45 -0500

    Update test url

--- client_test.js
+++ client_test.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
  
 const heroku = 'web-socket-playground.herokuapp.com'
 const local = `localhost:8080`
-const ws = new WebSocket(`ws://${local}`);
+const ws = new WebSocket(`ws://${heroku}`);
  
 ws.on('open', function open() {
   ws.send('something');
