964625ffd07160cdb71ff66d63372256c1443d15
Arohan Dutt <arohandutt@gmail.com>
2020-12-13 21:44:26 -0500

    Correct link to index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 
 const server = express()
-.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.use((req, res) => res.sendFile('./index.html', { root: __dirname }))
 .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new WebSocket.Server({ server });
